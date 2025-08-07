c6af891963ae6d848ea3eb7f245668e76e7362d1
ALEXANDRE SIQUEIRA SOUZA COSTA - X0801681 <X0801681@santos.sp.gov.br>
2025-08-07 16:45:53 -0300

    Adicionando a função de listar os nomes inseridos

--- challenge-amigo-secreto_pt-main/app.js
+++ challenge-amigo-secreto_pt-main/app.js
@@ -1,1 +1,30 @@
-//O principal objetivo deste desafio é fortalecer suas habilidades em lógica de programação. Aqui você deverá desenvolver a lógica para resolver o problema.
+let listaNomes = [];
+
+
+function adicionarAmigo(){
+    console.log(listaNomes.length);
+    let i = listaNomes.length;
+
+    let nome = document.querySelector('input').value;
+    listaNomes.push(nome);
+    console.log(listaNomes);
+
+    
+    let lista = document.getElementById('listaAmigos');
+    let novoElemento = document.createElement('li');
+    let texto = document.createTextNode(`${listaNomes[i]}`);
+    novoElemento.appendChild(texto);
+    
+    lista.appendChild(novoElemento);
+}
+
+
+function limparCampo(){
+
+}
+
+
+function exibirNomes(tag, texto){
+    let campo = document.querySelector(tag);
+    campo.innerHTML = texto;
+}
